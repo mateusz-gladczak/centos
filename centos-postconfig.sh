92c0fa20e25383bc5763a881ab2e93add2ebdc18
mateusz-gladczak <mateusz@gladczak.com>
2020-03-17 17:00:03 +0100

    Update centos-postconfig.sh

--- centos-postconfig.sh
+++ centos-postconfig.sh
@@ -10,6 +10,8 @@ sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+echo bindkey  "^[[H"   beginning-of-line >> ~/.zshrc
+echo bindkey  "^[[F"   end-of-line >> ~/.zshrc
 mkdir ~/.ssh
 touch ~/.ssh/authorized_keys
 echo ssh-rsa ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDLBfDPMab8T41TEJXeoB9BlId+RL56cyK5V3P7vmYFVF6IX/ZfViek0V6og9srwYOhWg/t7T7Aiu/qMwz8pDchui/xHZsB0aBiiZc1fjmzUIs8WOFFGVSVj4/Xv900vSmRwvbbI6kLyrbW20RSJdNK8/CqNn+zSL/KUYKEeq6WHW7n3rMcNSWna9LityR8GUcu481e4CEMhB6jtDR9Q9idZ1Em1zSTCwt5dMg5M9JpHsVfGRFbCfB9GIvxdzcg+mljSWW9iraDIyAXAv6uc4ouxkdla11IRWRA+VhB10TUB+PbOYRbqbxJTO2nowDjJYpfZsYGz95QzERxNbbF2Dg4FcFhziWO48N58OwVMQXSNez7V0ja0vIIqZoHsZ41J/eRf1WDYd3BeB2PDA/8i8mYZ+ftCIL+AoWJSlLDnlVzAG/hAt20mfeR+u++mnmaOPHls6GXhvZpoDfmmhN0vVPt2/b92QwHmGfdKeDD8kEuA94vccm9zNJ5ucwzSWrmy68= 69:21:93:c4:df:f5:46:aa:42:fb:b4:68:d7:0a:5f:cf >> ~/.ssh/authorized_keys
